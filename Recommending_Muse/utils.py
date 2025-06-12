f1da2331b2bf76203b80d81f79d42b5e9083f188
UJohnnyU <jhonkevinmurillom@gmail.com>
2025-06-11 21:05:06 -0500

    Agregando Proyecto

--- Recommending_Muse/utils.py
+++ Recommending_Muse/utils.py
@@ -1,3 +1,7 @@
 # Este archivo proporciona funciones utilitarias para el proyecto
 # Asegúrate de guardarlo en la misma carpeta que app.py
 # No es necesario agregar contenido adicional en este momento.
+
+# Por favor, asegúrate de instalar las siguientes dependencias antes de ejecutar el proyecto:
+#Flask : pip install Flask
+#RDFLib : pip install rdflib
